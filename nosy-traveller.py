d52bf76d8094b7d66bdfca1186894c50b7f57b8b
Mário Ferreira <marioferreira2110@gmail.com>
2020-04-23 15:31:59 +0200

    Return list of open ports instead of priting them inside scan_target function

--- nosy-traveller.py
+++ nosy-traveller.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 import argparse
 import socket
@@ -11,18 +11,21 @@ def log_message(message, facility):
 
 
 def scan_target(address, ports):
+    open_ports = []
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
             s.connect((address, ports))
         except ConnectionError:
-            log_message(f"Port {ports} is closed", syslog.LOG_INFO)
+            pass
         else:
-            log_message(f"Port {ports} is open", syslog.LOG_INFO)
+            open_ports.append(ports)
 
-        log_message("Scan finished", syslog.LOG_INFO)
+        return open_ports
 
 
 def main():
+    # Parse command line arguments
     parser = argparse.ArgumentParser(description="A simple port scanner")
     parser.add_argument("-a", "--address",
                         help="target's ip address", required=True)
@@ -33,11 +36,20 @@ def main():
     address = args.address
     ports = args.ports
 
+    # Connect to syslog server
     syslog.openlog(logoption=syslog.LOG_PID, facility=syslog.LOG_USER)
 
+    # Scan ports
     log_message(f"Scanning {address}", syslog.LOG_INFO)
-    scan_target(args.address, int(ports))
+    open_ports = scan_target(args.address, int(ports))
 
+    if len(open_ports) > 0:
+        for port in open_ports:
+            log_message(f"Port {ports} is open", syslog.LOG_INFO)
+    else:
+        log_message("All ports are closed", syslog.LOG_INFO)
+
+    log_message(f"Scan finished", syslog.LOG_INFO)
     exit(0)
 
 
